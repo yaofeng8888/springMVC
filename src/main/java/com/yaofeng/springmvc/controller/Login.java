f559fdf3e633453d3fbedaf9e3a567117364ed1a
yaofeng8888 <461563387@qq.com>
2018-01-11 21:36:19 +0800

    配置spring-*的xml文件，为后期的分层配置开头

--- src/main/java/com/yaofeng/springmvc/controller/Login.java
+++ src/main/java/com/yaofeng/springmvc/controller/Login.java
@@ -1,6 +1,7 @@
 package com.yaofeng.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,5 +18,13 @@ public class Login {
 	public String login(LoginUser user) {
 		System.out.println(user.getUsername()+"----------"+user.getPassword());
 		return "success";
+	}
+	
+	
+	@RequestMapping(value="/${userid}",method=RequestMethod.GET)
+	public LoginUser getById(@PathVariable("userid")String id) {
+		System.out.println("id="+id);
+		return null;
+		
 	}
 }
