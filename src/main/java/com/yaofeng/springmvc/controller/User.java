d2c9aee3f959bf288e3f940507371cd569cec49e
yaofeng8888 <461563387@qq.com>
2018-01-11 16:56:50 +0800

    mvc入门之hello mvc

---
+++ src/main/java/com/yaofeng/springmvc/controller/User.java
@@ -0,0 +1,18 @@
+package com.yaofeng.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class User {
+
+	//»Î√≈≥Ã–Ú
+	@ResponseBody
+	@RequestMapping(value="/hi")
+	
+	public String hello() {
+		return "hello mvc";
+	}
+
+}
