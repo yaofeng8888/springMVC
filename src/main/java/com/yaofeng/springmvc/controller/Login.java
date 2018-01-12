b477a3e3703e069438b7815a09c5b279bc6ad56a
yaofeng8888 <461563387@qq.com>
2018-01-12 19:43:25 +0800

    控制器原生的API,HttpServletRequest ,HttpServletResponse,HttpSession

--- src/main/java/com/yaofeng/springmvc/controller/Login.java
+++ src/main/java/com/yaofeng/springmvc/controller/Login.java
@@ -1,18 +1,27 @@
 package com.yaofeng.springmvc.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.springframework.http.HttpRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.alibaba.fastjson.JSON;
+
 @Controller
 public class Login {
 
 	/*@RequestMapping(value="/" ,method=RequestMethod.GET)
+	 * 原生的API
 	public String hello(HttpServletRequest request) {
 		request.setAttribute("name", "yaofeng");
 		return "login";
@@ -22,6 +31,21 @@ public class Login {
 		model.addAttribute("name", "hyy");
 		return "login";
 	}
+	
+	@RequestMapping(value="/json" ,method=RequestMethod.GET)
+	public void json(HttpServletRequest request,HttpServletResponse response,
+			HttpSession session) throws Exception {
+		Map<String,Object>map = new HashMap<String,Object>();
+		map.put("name","张三");
+		map.put("age", 13);
+		PrintWriter out = response.getWriter();
+		String jsonString = JSON.toJSONString(map);
+		out.print(jsonString);
+		out.flush();
+		out.close();
+	}
+	
+	
 	
 	@RequestMapping(value="/user/login",method=RequestMethod.POST)
 	public String login(User user) {
